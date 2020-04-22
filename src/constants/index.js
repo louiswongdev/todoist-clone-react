b8b594f06d4518aeecb936c747d907b8d01eb0a5
Louis <louis@Louiss-Mac-mini.local>
2020-04-21 21:49:22 -0400

    Added setSelectedProject to Sidebar projects and tweaked CSS for sidebar

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const collatedTasks = [
   { key: 'INBOX', name: 'Inbox' },
-  { key: 'Today', name: 'Today' },
+  { key: 'TODAY', name: 'Today' },
   { key: 'NEXT_7', name: 'Next 7 Days' },
 ];
